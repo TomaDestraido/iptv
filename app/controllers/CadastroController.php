aab4bd2318ffacdb54083ce21b2aa7d83a7fee27
TomaDestraido <tomadestraido@gmail.com>
2017-07-15 20:33:37 -0300

    Autenticação do usuario no cadastro

--- app/controllers/CadastroController.php
+++ app/controllers/CadastroController.php
@@ -1,38 +1,37 @@
 <?php
 
-class CadastroController extends \HXPHP\System\Controller
-{
+class CadastroController extends \HXPHP\System\Controller {
+
     public function __construct($configs) {
         parent::__construct($configs);
         $this->load(
-        'Services\Auth',
-        $configs->auth->after_login,        
-        $configs->auth->after_logout,        
-         TRUE
-                );
-                $this->auth->redirectCheck(TRUE);
+                'Services\Auth', $configs->auth->after_login, $configs->auth->after_logout, TRUE
+        );
+        $this->auth->redirectCheck(TRUE);
     }
 
-    public function cadastrarAction()
-	{
-		$this->view->setFile('index');
+    public function cadastrarAction() {
+        $this->view->setFile('index');
 
-		$this->request->setCustomFilters(array(
-			'email' => FILTER_VALIDATE_EMAIL
-		));
+        $this->request->setCustomFilters(array(
+            'email' => FILTER_VALIDATE_EMAIL
+        ));
 
-		$post = $this->request->post();
+        $post = $this->request->post();
 
-		if (!empty($post)) {
-			$cadastrarUsuario = User::cadastrar($post);
+        if (!empty($post)) {
+            $cadastrarUsuario = User::cadastrar($post);
 
-			if ($cadastrarUsuario->status === false) {
-				$this->load('Helpers\Alert', array(
-					'danger',
-					'Ops! Não foi possível efetuar seu cadastro. <br> Verifique os erros abaixo:',
-					$cadastrarUsuario->errors
-				));
-			}
-		}
-	}
+            if ($cadastrarUsuario->status === false) {
+                $this->load('Helpers\Alert', array(
+                    'danger',
+                    'Ops! Não foi possível efetuar seu cadastro. <br> Verifique os erros abaixo:',
+                    $cadastrarUsuario->errors
+                ));
+            } else {
+                $this->auth->login($cadastrarUsuario->user->id, $cadastrarUsuario->user->username);
+            }
+        }
+    }
+
 }
