72953da5d4df3f353cb7c484ac6717d20720bb86
TomaDestraido <tomadestraido@gmail.com>
2017-07-15 20:27:15 -0300

    Add serviço Auth no construtor

--- app/controllers/CadastroController.php
+++ app/controllers/CadastroController.php
@@ -2,7 +2,18 @@
 
 class CadastroController extends \HXPHP\System\Controller
 {
-	public function cadastrarAction()
+    public function __construct($configs) {
+        parent::__construct($configs);
+        $this->load(
+        'Services\Auth',
+        $configs->auth->after_login,        
+        $configs->auth->after_logout,        
+         TRUE
+                );
+                $this->auth->redirectCheck(TRUE);
+    }
+
+    public function cadastrarAction()
 	{
 		$this->view->setFile('index');
 
